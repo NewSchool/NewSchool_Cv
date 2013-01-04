42f745b36f07153dc60604834dbd408a97513359
AmadoRamos <amado0529@gmail.com>
2013-01-03 20:45:55 -0500

    git ignore

--- apps/database/admin.py
+++ apps/database/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from apps.database.models import *
 
+
 admin.site.register(institucion)
 admin.site.register(jornada)
 admin.site.register(grado)
 admin.site.register(nivel)
+admin.site.register(Perfil)
--- apps/database/models.py
+++ apps/database/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Perfil(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    direccion = models.CharField(max_length=250, blank=True)
+    telefono = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+		verbose_name_plural = "perfiles"
 
 class institucion(models.Model):
 	nombre		= models.CharField(max_length=200)
